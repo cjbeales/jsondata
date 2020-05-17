429589ec9e5c806c6196ad685dba015de057ccaf
Connor Beales <connor.beales@oeconnection.com>
2020-05-17 12:14:16 +0100

    Random generates on load, adding event listener to generate button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-var jsonData = [];
-
 // Json Request
 var request = new XMLHttpRequest();
 request.open('GET', '/assets/json/quotes.json', true);
@@ -8,14 +6,33 @@ request.onload = function () {
   if (this.status >= 200 && this.status < 400) {
     // Success!
     console.log("success");
-
     var data = JSON.parse(this.response);
+    var quotes = data.quotes;
 
-    let quotes = data.quotes;
+    (function getQuote() {
+      var jsonData = [];
 
-    for (let i = 0; i < quotes.length; i++) {
-      jsonData.push(quotes[i]);
-    }
+      for (let i = 0; i < quotes.length; i++) {
+        let authors = quotes[i].author;
+        jsonData.push(quotes[i]);
+      }
+
+      let randomQuote = jsonData[Math.floor(Math.random() * jsonData.length)];
+      let quoteContainer = document.getElementById('quoteContainer');
+      let quoteDiv = document.createElement('div');
+      quoteDiv.className = "quote";
+      let quoteText = document.createElement('h2');
+      quoteText.innerHTML = randomQuote.quote;
+      let quoteAuthor = document.createElement('p');
+      quoteAuthor.innerHTML = randomQuote.author;
+
+      // Appends
+      quoteDiv.appendChild(quoteAuthor);
+      quoteDiv.appendChild(quoteText);
+      quoteContainer.appendChild(quoteDiv);
+
+      document.getElementById('generate').addEventListener('click', getQuote)
+    }());
 
   } else {
     // We reached our target server, but it returned an error
@@ -28,5 +45,3 @@ request.onerror = function () {
 };
 
 request.send();
-
-console.log(jsonData);
